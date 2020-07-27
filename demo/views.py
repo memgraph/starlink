86590fa52db0c93448c44df0993b7f59bf115813
Ivan Despot <ivan.g.despot@gmail.com>
2020-07-27 16:06:23 +0200

    Cities added to map

--- demo/views.py
+++ demo/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 from demo.database import Memgraph
 import demo.data.db_operations as db_operations
 from django.template import loader
+from django.shortcuts import render 
 import json
-
 
 
 def index(request):
@@ -17,50 +17,20 @@ def index(request):
 
     for sat in satellites:
         s = sat['n']
-        #print(s.properties)
         marker = [s.properties['x'], s.properties['y']]
         sat_markers.append(marker)
 
     for city in cities:
         c = city['n']
-        #print(c.properties)
         marker = [c.properties['x'], s.properties['y']]
         city_markers.append(marker)
 
-    #print(sat_markers)
-    for i in city_markers:
-        print(i)
-
-    html = """<html>
-    <head>
-        <link rel='stylesheet' href='https://unpkg.com/leaflet@1.6.0/dist/leaflet.css' />
-        <script src='https://unpkg.com/leaflet@1.6.0/dist/leaflet.js'></script>
-        <style>
-            #map {position: absolute; top: 0; bottom:0; left:0; right: 0}
-        </style>
-    </head>
-    <body>
-        <div id = "map"></div>
-        <script>
-            var map = L.map('map').setView([0,0], 1);
-            L.tileLayer("https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=cYtmZZ4gfz1cXNCBs8r4",{
-                attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-            }).addTo(map);
-            var mark;
-            var city_markers = {{city_markers|tojson}};
-            for (mark in city_markers){
-                var marker = L.marker(mark).addTo(map);
-            }
-            
-        </script>
-    </body>
-</html>
-    """
     json_cities = json.dumps(city_markers)
 
+    for i in json_cities:
+        print(i)
     template = loader.get_template('demo/demo.html')
     context = {
         'city_markers': json_cities,
     }
-    return HttpResponse(template.render(context, request))
-    #return HttpResponse(html)
+    return render(request, "demo/demo.html", {"city_markers": json_cities}) 
