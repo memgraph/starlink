c032f3ac580a0079f3dcc4356124350dca872fcd
Ivan Despot <ivan.g.despot@gmail.com>
2020-07-28 14:03:52 +0200

    AJAX communication added

--- demo/urls.py
+++ demo/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('post/ajax/json_satellites', views.postSatellites, name = "json_satellites"),
 ]
--- demo/views.py
+++ demo/views.py
@@ -4,6 +4,7 @@ import demo.data.db_operations as db_operations
 from django.template import loader
 from django.shortcuts import render
 import json
+from django.http import JsonResponse
 import demo.data.db_connection as db_connection
 
 
@@ -27,3 +28,17 @@ def index(request):
 
     template = loader.get_template('demo/demo.html')
     return render(request, "demo/demo.html", {"city_markers": json_cities, "sat_markers": json_satellites})
+
+
+def postSatellites(request):
+    db = Memgraph()
+
+    satellites = []
+    json_satellites = []
+
+    satellites = db_connection.fetch_satellites(db)
+
+    json_satellites = json.dumps(
+        db_connection.satellite_json_format(satellites))
+
+    return JsonResponse({"json_satellites": json_satellites}, status=200)
