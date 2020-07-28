b5e724ec08df82635ad9998db88ae31b1360cd1f
nmotocic <48250159+nmotocic@users.noreply.github.com>
2020-07-28 12:09:34 +0200

    Bug fix

--- demo/data/city.py
+++ demo/data/city.py
@@ -1,8 +1,8 @@
 
-
 class City():
-    def __init__(self, id, x0, y0, z0):
+    def __init__(self, id, x0, y0, z0, name):
         self.id = id
         self.x = x0
         self.y = y0
         self.z = z0
+        self.name = name
