edc447be28ae4ed22afac9dfef6de642521c00ad
g-despot <ivan.g.despot@outlook.com>
2020-08-07 17:49:09 +0200

    shortest path only uses nodes with label Satellite

--- demo/data/db_operations.py
+++ demo/data/db_operations.py
@@ -8,15 +8,15 @@ def import_all_cities(db):
 
 def import_data(tx, city1, city2):
     print(f"{utils.bcolors.OKGREEN}Web DB update START{utils.bcolors.ENDC}")
-          
+
     results = {}
 
     command = "MATCH (s:Satellite) RETURN s;"
-    results[0] = tx.run(command) 
+    results[0] = tx.run(command)
 
     command = "MATCH p=(c1:City { id: '" + str(city1) + \
         "'})-[rs *wShortest (e, n | e.transmission_time) total_transmission_time]-(c2:City { id: '" + str(
-            city2) + "'}) RETURN nodes(p), rs;"
+        city2) + "'}) WHERE ALL(x IN nodes(p)[1..-1] WHERE (x:Satellite))  RETURN nodes(p), rs;"
     results[2] = tx.run(command)
 
     command = "MATCH (s1:Satellite)-[r]-(s2:Satellite) RETURN r, s1, s2;"
