042ecd923b07543efe061e6419d1ffa5f410248c
g-despot <ivan.g.despot@outlook.com>
2020-08-07 10:07:22 +0200

    transmission time added to bootstrap, cities fixed

--- starlink_simulator/constants.py
+++ starlink_simulator/constants.py
@@ -2,18 +2,21 @@ SIZE_X = 360    # degrees [-180,180]
 SIZE_Y = 180    # degrees [-90,90]
 SAT_ALT = 550   # KM
 NUM_ORB = 24    # number of orbits for Starlink Phase 1 (FCC filing Nov 2018)
-NUM_OBJ = 10    # number of satellites per orbits for Starlink Phase 1 (FCC filing Nov 2018)
+NUM_OBJ = 20    # number of satellites per orbits for Starlink Phase 1 (FCC filing Nov 2018)
 
 """Starlink constellation: 24 orbits, 33 satellites per orbit, orbital shell inclination of 63 degrees, altitude of 550 km"""
 #TLE_FILE = "starlink_simulator/imports/tle_1"
 
 """Starlink constellation: 24 orbits, 10 satellites per orbit, orbital shell inclination of 63 degrees, altitude of 550 km"""
-TLE_FILE = "starlink_simulator/imports/tle_2"
+#TLE_FILE = "starlink_simulator/imports/tle_2"
+
+"""Starlink constellation: 24 orbits, 20 satellites per orbit, orbital shell inclination of 63 degrees, altitude of 550 km"""
+TLE_FILE = "starlink_simulator/imports/tle_3"
 
 V_LASER_VACUUM = 2.99792458E+8
 V_LASER_CABLE = 2.04190477E+8
 V_RADIO = 2.99792458E+8
-VIEW_ANGLE = 45
+VIEW_ANGLE = 60
 CITIES_FILE = "starlink_simulator/imports/cities.csv"
 OPTICAL_FILE = "starlink_simulator/imports/latencies.csv"
 EDGE_CONNECTED = True
