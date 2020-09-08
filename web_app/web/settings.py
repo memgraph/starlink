421366df7b9df04f19c6061f14d053757e0da3d7
g-despot <ivan.g.despot@outlook.com>
2020-09-08 10:15:32 +0200

    static files loading changes

--- web_app/web/settings.py
+++ web_app/web/settings.py
@@ -92,10 +92,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/demo/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'demo/static'),
+)
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 
